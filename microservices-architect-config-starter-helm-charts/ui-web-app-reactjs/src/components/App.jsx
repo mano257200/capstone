a3075c2793073b2178f240b68ffe1c9c88897f65
mano257200 <62204429+mano257200@users.noreply.github.com>
2023-09-12 11:41:34 +0530

    Update App.jsx

--- microservices-architect-config-starter-helm-charts/ui-web-app-reactjs/src/components/App.jsx
+++ microservices-architect-config-starter-helm-charts/ui-web-app-reactjs/src/components/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   const [displayData, setDisplayData] = useState({ url: '' });
 
   // Replace the dynamic IP fetch with the static IP address
-  const staticIP = '54.151.6.75';
+  const staticIP = '40.88.126.75';
 
   const handleApiCall = async (e) => {
     let url = `http://${staticIP}:32470/${e.target.name}`;
